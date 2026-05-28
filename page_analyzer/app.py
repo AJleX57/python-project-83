dec3451fddb7b144e461fe46de83da59e660ad81
AJleX57 <s.alex06.a@gmail.com>
2026-05-29 02:55:53 +0300

    Add main page template

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,13 +1,13 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 from dotenv import load_dotenv
-
+ 
 load_dotenv()
-
+ 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-
-
+ 
+ 
 @app.route('/')
 def index():
-    return 'Hello, World!'
+    return render_template('index.html')
